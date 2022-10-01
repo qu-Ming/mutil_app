67ad49a4d22115a0de2f41bb1b24928b12a0293d
Minh <minhdagudboiz@gmail.com>
2022-10-01 20:20:17 +0700

    ui

--- lib/pages/todo_page/add_todo_page.dart
+++ lib/pages/todo_page/add_todo_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mutil_app/components/text_component.dart';
+import 'package:mutil_app/components/textfield_component.dart';
 import 'package:mutil_app/utils/const/app_colors.dart';
+import 'package:mutil_app/utils/const/app_dimens.dart';
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({Key? key}) : super(key: key);
@@ -9,6 +12,8 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +25,23 @@ class _AddTodoPageState extends State<AddTodoPage> {
               Navigator.pop(context);
             },
             icon: const Icon(Icons.chevron_left)),
+      ),
+      body: Column(
+        children: [
+          const Align(
+            alignment: Alignment.topLeft,
+            child: TextComponent(
+              text: 'Create a task\nTodo',
+              colorText: AppColors.colorPink,
+              fontWeight: FontWeight.bold,
+              textSize: AppDimens.text_size_20,
+            ),
+          ),
+          TextFieldComponent(
+              text: 'Tieu de', textEditingController: titleController),
+          TextFieldComponent(
+              text: 'Noi dung', textEditingController: titleController),
+        ],
       ),
     );
   }
