90190c2d02de3c921c591590f2e4b1b6b86f846e
Quang Minh <minhdagudboiz@gmail.com>
2022-10-06 08:24:26 +0700

    model

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,1 +1,9 @@
-class TodoModel {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TodoModel {
+  String? title;
+  String? content;
+  Timestamp? dayCreate;
+
+  TodoModel({this.title, this.content, this.dayCreate});
+}
