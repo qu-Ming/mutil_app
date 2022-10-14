ccfab1821f157dc8e0fda92a8e6623af4ca298e9
Quang Minh <minhdagudboiz@gmail.com>
2022-10-14 16:48:46 +0700

    85% chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mutil_app/pages/bottom_navigator.dart';
+import 'package:mutil_app/pages/chat_page/chat_page.dart';
+import 'package:mutil_app/pages/chat_page/security_page.dart';
 
 import 'firebase_options.dart';
 
--- lib/pages/chat_page/security_page.dart
+++ lib/pages/chat_page/security_page.dart
@@ -15,36 +15,36 @@ TextEditingController security = TextEditingController();
 class _SecurityPageState extends State<SecurityPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: AppColors.colorPink4,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 30.0),
-              decoration: BoxDecoration(
-                  color: AppColors.colorWhite,
-                  borderRadius: BorderRadius.circular(24)),
-              child: TextFormField(
-                controller: security,
-                cursorColor: AppColors.colorBlack,
-                decoration: const InputDecoration(
-                    hintText: 'Nhập pass',
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: AppColors.colorPink,
-                    ),
-                    border: InputBorder.none),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: AppColors.colorPink4,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 30.0, vertical: 60),
+                decoration: BoxDecoration(
+                    color: AppColors.colorWhite,
+                    borderRadius: BorderRadius.circular(24)),
+                child: TextFormField(
+                  controller: security,
+                  cursorColor: AppColors.colorBlack,
+                  decoration: const InputDecoration(
+                      hintText: 'Nhập pass',
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: AppColors.colorPink,
+                      ),
+                      border: InputBorder.none),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
+              ElevatedButton(
                   style: ButtonStyle(
                       backgroundColor:
                           MaterialStateProperty.all(AppColors.colorPink)),
                   onPressed: () {
-                    if (security.text == "m0912") {
+                    if (security.text == "m912") {
                       String pass = "minh";
                       Navigator.push(
                           context,
@@ -52,7 +52,7 @@ class _SecurityPageState extends State<SecurityPage> {
                               builder: (context) => ChatPage(
                                     pass: pass,
                                   )));
-                    } else if (security.text == "n0912") {
+                    } else if (security.text == "n912") {
                       String pass = "ngan";
                       Navigator.push(
                           context,
@@ -62,9 +62,9 @@ class _SecurityPageState extends State<SecurityPage> {
                                   )));
                     }
                   },
-                  child: const Text('Xác nhận')),
-            )
-          ],
-        ));
+                  child: const Text('Xác nhận'))
+            ],
+          )),
+    );
   }
 }
